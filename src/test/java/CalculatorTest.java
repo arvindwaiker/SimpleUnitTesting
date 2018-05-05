3b6c922c0390476b3a94a579f38d314013618ee7
Arvind Waiker <arvind@Arvinds-MacBook-Pro.local>
2018-05-05 15:41:03 +0530

    Initial Commit

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        Assert.assertEquals(5,calculator.add(2,3));
+        Assert.assertEquals(7,calculator.add(2,5));
+        Assert.assertEquals(8,calculator.add(-2,10));
+    }
+}
